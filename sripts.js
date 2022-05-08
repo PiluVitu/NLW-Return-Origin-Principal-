8b90458ed71acb0c601c624881577b633db6a8af
Paulo Victor <Paulo.tspi@gmail.com>
2022-05-08 02:00:53 -0300

    nlw complete

--- sripts.js
+++ sripts.js
@@ -3,7 +3,64 @@ window.addEventListener('scroll', onScroll)
 function onScroll(){
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+  activeMenuOnTheCurrentSection(home)
+  activeMenuOnTheCurrentSection(services)
+  activeMenuOnTheCurrentSection(about)
+  activeMenuOnTheCurrentSection(contact)
+  
 }
+
+function activeMenuOnTheCurrentSection(section){
+  const targetLine = scrollY + innerHeight/2;
+
+  // Verificar se passou da linha
+  // Quais são os dados necessários ?  
+  
+    //Ver onde o topo do elemento está
+  const sectionTop = section.offsetTop;
+    //Ver a altura do elemento
+  const sectionHeight = section.offsetHeight;   
+  
+  //O topo da seção chegou ou passou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  //console.log('O topo da seção passou da linha ?', sectionTopReachOrPassedTargetLine);
+
+  // Verificar se a base está abaixo da linha alvo 
+  const sectionEndsAt = sectionTop + sectionHeight;
+
+  
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+  
+  
+  //console.log('O fundo da sessão passou da linha ? ' ,sectionEndPassedTargetLine);
+
+  // Limites da sessão 
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine &&!sectionEndPassedTargetLine
+
+  const sectionID = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionID}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function showNavOnScroll(){
   if(scrollY > 75){
